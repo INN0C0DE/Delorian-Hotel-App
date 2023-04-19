8e8c107c39c8ee2725c3a395ac00b55a643b9032
INN0C0DE <trickted@gmail.com>
2023-04-19 23:08:31 +0800

    fixed welcome page

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -8,37 +8,196 @@ class WelcomePage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(13.0),
           child: Column(
             children: [
-              Image.asset('images/hotelroom1.jpg'),
               const Text(
-                'Amazing Hotel Rooms',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                'WELCOME TO THE DELORIAN HOTEL,',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.brown,
+                ),
               ),
               const SizedBox(
                 height: 20,
               ),
+              Image.asset('images/hotel_building.jpg'),
+              const SizedBox(
+                height: 10,
+              ),
               const Text(
-                '          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quam turpis, pretium et dignissim in, feugiat ut urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet eros aliquam diam tempor luctus sed at risus. Suspendisse pulvinar elit in erat ornare, vel lacinia urna dignissim. Mauris sed quam quis orci placerat suscipit id sit amet libero. Sed pretium, justo sit amet eleifend ultricies, dui orci ultrices ex, vitae mollis ante leo et erat. Aliquam erat volutpat. Sed luctus aliquet tempus. Integer dictum suscipit urna, sed dictum augue commodo vitae. Aliquam aliquam metus ante.',
+                '          Welcome to the Delorian Hotel, where luxury and comfort meet! We are thrilled to have you as our guest and look forward to providing you with an unforgettable experience. Our hotel is designed to cater to all of your needs, whether you are here for business or leisure. From our state-of-the-art facilities to our attentive staff, we are committed to ensuring that your stay with us is nothing short of extraordinary.',
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              //Bonding
+              const Text(
+                'QUALITY TIME WITH LOVE ONES:',
                 style: TextStyle(
-                  fontSize: 18,
+                  fontSize: 27,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.brown,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Image.asset('images/hotel_family.jpg'),
+              Image.asset('images/hotel_friends.jpg'),
+              Image.asset('images/hotel_couple.jpg'),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                '          At the Delorian Hotel, we believe that there is nothing more important than spending quality time with family, friends, and loved ones. That is why we have created a warm and welcoming atmosphere that is perfect for bonding and creating memories that will last a lifetime.',
+                style: TextStyle(
+                  fontSize: 20,
                 ),
                 textAlign: TextAlign.justify,
               ),
-              Image.asset('images/hotelroom2.jpg'),
+              const SizedBox(
+                height: 50,
+              ),
               const Text(
-                '          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quam turpis, pretium et dignissim in, feugiat ut urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet eros aliquam diam tempor luctus sed at risus. Suspendisse pulvinar elit in erat ornare, vel lacinia urna dignissim. Mauris sed quam quis orci placerat suscipit id sit amet libero. Sed pretium, justo sit amet eleifend ultricies, dui orci ultrices ex, vitae mollis ante leo et erat. Aliquam erat volutpat. Sed luctus aliquet tempus. Integer dictum suscipit urna, sed dictum augue commodo vitae. Aliquam aliquam metus ante.',
+                'QUALITY COMFORT:',
                 style: TextStyle(
-                  fontSize: 18,
+                  fontSize: 27,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.brown,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Image.asset('images/hotel_bedsheets.jpg'),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                '          At the Delorian Hotel, we know that a good nights sleep is essential to our guests overall comfort and satisfaction during their stay. That is why we have invested in the highest quality bedding and mattresses to ensure that our guests have the most comfortable sleeping experience possible.',
+                style: TextStyle(
+                  fontSize: 20,
                 ),
                 textAlign: TextAlign.justify,
               ),
-              Image.asset('images/hotelroom3.jpg'),
+              const SizedBox(
+                height: 50,
+              ),
+              // Foods
               const Text(
-                '          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quam turpis, pretium et dignissim in, feugiat ut urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet eros aliquam diam tempor luctus sed at risus. Suspendisse pulvinar elit in erat ornare, vel lacinia urna dignissim. Mauris sed quam quis orci placerat suscipit id sit amet libero. Sed pretium, justo sit amet eleifend ultricies, dui orci ultrices ex, vitae mollis ante leo et erat. Aliquam erat volutpat. Sed luctus aliquet tempus. Integer dictum suscipit urna, sed dictum augue commodo vitae. Aliquam aliquam metus ante.',
+                'QUALITY FOOD & SERVICE:',
                 style: TextStyle(
-                  fontSize: 18,
+                  fontSize: 27,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.brown,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Image.asset('images/hotel_food2.jpg'),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                '          At the Delorian Hotel, we believe that every guest deserves to have fun and unwind during their stay, which is why we offer a range of entertainment and recreational activities to keep everyone entertained. Our outdoor pool is the perfect place to relax and soak up the sun while enjoying a refreshing dip in the water.',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Image.asset('images/hotel_food.jpg'),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                '          Our signature restaurant offers an upscale dining experience, where guests can indulge in a menu of contemporary cuisine with a focus on local and seasonal ingredients. We also have a casual all-day dining restaurant that serves a range of international and local dishes, perfect for those who want to enjoy a relaxed meal with family or friends.',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Image.asset('images/hotel_service.jpg'),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                '          For those who prefer to dine in the comfort of their own room, we offer a 24-hour room service menu that features a range of dishes to suit all tastes and dietary requirements. Whether you are looking for a hearty breakfast, a quick snack, or a late-night bite, our room service team is always on hand to cater to your needs.',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              // POOL AND RELAXATION
+              const Text(
+                'POOL AND RELAXATION:',
+                style: TextStyle(
+                  fontSize: 27,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.brown,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Image.asset('images/hotel_pool3.jpg'),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                '          At the Delorian Hotel, we believe that every guest deserves to have fun and unwind during their stay, which is why we offer a range of entertainment and recreational activities to keep everyone entertained. Our outdoor pool is the perfect place to relax and soak up the sun while enjoying a refreshing dip in the water.',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text(
+                '          Our pool is heated to the perfect temperature, making it enjoyable year-round, and our poolside lounge chairs and cabanas provide the perfect spot to unwind and relax. We also have a poolside bar that serves refreshing drinks and light bites, allowing you to stay refreshed and energized throughout your poolside experience.',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              // ENTERTAINMENT
+              const Text(
+                'ENTERTAINMENT:',
+                style: TextStyle(
+                  fontSize: 27,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.brown,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Image.asset('images/hotel_playing.jpg'),
+              Image.asset('images/hotel_console.jpg'),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                '          If you are looking for more entertainment options, we also have a range of activities and amenities to keep you occupied. Our fitness center is equipped with state-of-the-art equipment, perfect for those who want to stay active during their stay. We also have a game room with pool tables, foosball, and other games, providing endless hours of fun for guests of all ages.',
+                style: TextStyle(
+                  fontSize: 20,
                 ),
                 textAlign: TextAlign.justify,
               ),

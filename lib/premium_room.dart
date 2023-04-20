5d3110ab1352d292c3e2a19aa1bc3db3687c44df
INN0C0DE <trickted@gmail.com>
2023-04-20 20:32:55 +0800

    carousel slider for premium rooms

--- lib/premium_room.dart
+++ lib/premium_room.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_pro_nullsafety/carousel_pro_nullsafety.dart';
 import 'package:flutter/material.dart';
 
 class PremiumRoomPage extends StatelessWidget {
@@ -7,7 +8,50 @@ class PremiumRoomPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: const Color(0xFFA9907E),
         title: const Text('Delorian Hotel: Premium Rooms'),
+      ),
+      body: const PremiumRoomSlider(),
+    );
+  }
+}
+
+class PremiumRoomSlider extends StatefulWidget {
+  const PremiumRoomSlider({super.key});
+
+  @override
+  State<PremiumRoomSlider> createState() => _PremiumRoomSliderState();
+}
+
+class _PremiumRoomSliderState extends State<PremiumRoomSlider> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: ListView(
+            children: [
+              SizedBox(
+                height: 280,
+                width: double.infinity,
+                child: Carousel(
+                  dotColor: const Color(0xFFA9907E),
+                  dotIncreasedColor: Colors.white,
+                  dotSize: 6.0,
+                  dotSpacing: 15.0,
+                  dotPosition: DotPosition.bottomCenter,
+                  images: [
+                    Image.asset('images/hotelroom1.jpg', fit: BoxFit.cover),
+                    Image.asset('images/hotel_pool3.jpg', fit: BoxFit.cover),
+                    Image.asset('images/hotel_food2.jpg', fit: BoxFit.cover),
+                    Image.asset('images/hotel_service.jpg', fit: BoxFit.cover),
+                    Image.asset('images/hotel_console.jpg', fit: BoxFit.cover)
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
